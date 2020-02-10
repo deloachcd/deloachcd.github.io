79de58347c7d6948bf05da7b845d97796db8cdd5
Chandler DeLoach <deloach.cdd@gmail.com>
2020-02-09 20:23:01 -0700

    changes

--- .websrc/websrc/populator.js
+++
@@ -1,8 +0,0 @@
-remote_metafile = "https://deloachcd.github.io/meta.json"
-
-metadata = fetch(remote_metafile)
-    .then((response) => {
-        return response.json();
-    });
-
-console.log(metadata);
