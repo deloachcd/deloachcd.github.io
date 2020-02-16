68d44d650f63931f12224bee5e78cb0167e05577
Chandler DeLoach <deloach.cdd@gmail.com>
2020-02-16 11:42:27 -0700

    changes

--- websrc/populator.js
+++ websrc/populator.js
@@ -3,6 +3,8 @@ let remote_metafile = "https://deloachcd.github.io/websrc/meta.json"
 let albums_table = document.getElementById("albums");
 let sidenav = document.getElementById("sidenav");
 let sidenav_tags_list = []
+let album_display_list = []
+let table_maxwidth = 5
 
 function sidenav_create_tag(tagliteral) {
     /*<div class="tag">
@@ -23,7 +25,17 @@ function sidenav_create_tag(tagliteral) {
     sidenav.appendChild(new_element);
 }
 
-function main() {
+function renderSidenav(tags) {
+    tags.forEach((tagname, index) => {
+        if (sidenav_tags_list.indexOf(tagname) == -1) {
+            // tag not in sidenav
+            sidenav_create_tag(tagname)
+            sidenav_tags_list.push(tagname)
+        }
+    })
+}
+
+function initial() {
     fetch(remote_metafile)
         .then((response) => {
             return response.json();
@@ -31,16 +43,10 @@ function main() {
         .then((json_metadata) => {
             var collection = json_metadata.collection;
             collection.forEach((entry, index) => {
-                console.log(`${entry.album} - ${entry.artist}`)
-                entry.tags.forEach((tagname, index) => {
-                    if (sidenav_tags_list.indexOf(tagname) == -1) {
-                        // tag not in sidenav
-                        sidenav_create_tag(tagname)
-                        sidenav_tags_list.push(tagname)
-                    }
-                })
+                // Populate sidenav with tags
+                renderSidenav(entry.tags);
             })
         });
 }
 
-main();
+initial();
