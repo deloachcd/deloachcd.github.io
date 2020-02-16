720b6a80343f59802cb97dd23afd57f4b0a2694e
Chandler DeLoach <deloach.cdd@gmail.com>
2020-02-16 11:23:37 -0700

    changes

--- websrc/populator.js
+++ websrc/populator.js
@@ -1,13 +1,46 @@
-remote_metafile = "https://deloachcd.github.io/websrc/meta.json"
+// global constants
+let remote_metafile = "https://deloachcd.github.io/websrc/meta.json"
+let albums_table = document.getElementById("albums");
+let sidenav = document.getElementById("sidenav");
+let sidenav_tags_list = []
 
-fetch(remote_metafile)
-    .then((response) => {
-        return response.json();
-    })
-    .then((json_metadata) => {
-        console.log(json_metadata)
-        collection = json_metadata.collection;
-        collection.forEach((entry, index) => {
-            console.log(`${entry.album} - ${entry.artist}`)
+function sidenav_create_tag(tagliteral) {
+    /*<div class="tag">
+      <input type="checkbox" name="tag_focus" value="false"></input>
+      <label for="tag_focus">focus</label>
+    </div>*/
+    var new_element = document.createElement("div");
+    new_element.setAttribute("class", "tag");
+    checkbox = document.createElement("input")
+    checkbox.setAttribute("type", "checkbox");
+    checkbox.setAttribute("name", `checkbox_${tagliteral}`);
+    checkbox.setAttribute("value", false);
+    label = document.createElement("label");
+    label.setAttribute("for", `checkbox_${tagliteral}`);
+    label.appendChild(document.createTextNode(`${tagliteral}`))
+    new_element.appendChild(checkbox);
+    new_element.appendChild(label);
+    sidenav.appendChild(new_element);
+}
+
+function main() {
+    fetch(remote_metafile)
+        .then((response) => {
+            return response.json();
         })
-    });
+        .then((json_metadata) => {
+            var collection = json_metadata.collection;
+            collection.forEach((entry, index) => {
+                console.log(`${entry.album} - ${entry.artist}`)
+                entry.tags.forEach((tagname, index) => {
+                    if (sidenav_tags_list.indexOf(tagname) == -1) {
+                        // tag not in sidenav
+                        sidenav_create_tag(tagname)
+                        sidenav_tags_list.push(tagname)
+                    }
+                })
+            })
+        });
+}
+
+main();
