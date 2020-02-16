4fffa2b6d107eda3ea1372fc2ec8b99710559ecc
Chandler DeLoach <deloach.cdd@gmail.com>
2020-02-16 10:48:15 -0700

    remove redundant websrc path

--- websrc/websrc/populator.js
+++
@@ -1,9 +0,0 @@
-remote_metafile = "https://deloachcd.github.io/websrc/meta.json"
-
-fetch(remote_metafile)
-    .then((response) => {
-        return response.json();
-    })
-    .then((json_metadata) => {
-        console.log(json_metadata)
-    });
